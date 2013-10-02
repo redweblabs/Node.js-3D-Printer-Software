c3115bfcfae9a923a364f70d9d01fa97dd56123e
redweblabs <kriss@Redweb-Innovation-MacBook.local>
2013-10-02 12:03:57 +0100

    Turned GCODE parser into a module
    Partly learning about modules, Partly because it was a little out of
    place in the main print.js file.

---
+++ GCODE/module.js
@@ -0,0 +1,121 @@
+
+function deconstruct(code){
+
+    var printData;
+
+    printData = code.split('\n');
+
+    var newData = [];
+
+    var cS = 0;
+
+    while(cS < printData.length){
+
+        var thisLine = printData[cS].split(";")[0].split(' ');
+
+        if(thisLine.length > 1){
+            var lL = 1;
+
+            var printObject = {};
+            printObject.commandCode = thisLine[0];
+            printObject.other = []
+
+
+            while(lL < thisLine.length){
+
+                if(thisLine[lL].indexOf('E') != -1){
+                    var tempChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('E')).split('E')[1], 10);
+    
+                    if(tempChunk < 4){
+                        tempChunk = 4;
+                    }
+                    printObject.extrusion = tempChunk;
+
+                } else if(thisLine[lL].indexOf('X') != -1){
+                    var xChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('X')).split('X')[1], 10);
+
+                    printObject.x = xChunk;
+                } else if(thisLine[lL].indexOf('Y') != -1){
+                    var yChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('Y')).split('Y')[1], 10);
+
+                    printObject.y = yChunk;
+                } else if(thisLine[lL].indexOf('Z') != -1){
+                    var zChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('Z')).split('Z')[1], 10);
+
+                    printObject.z = zChunk;
+                } else if(thisLine[lL].indexOf('F') != -1){
+                    var fChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('F')).split('F')[1], 10);
+
+                    printObject.feed = fChunk;
+                } else {
+
+                    if(thisLine[lL] !== " " && thisLine[lL] !== ""){
+                        printObject.other.push(thisLine[lL]);
+                    }
+
+                }
+
+                lL += 1;
+            }
+
+            newData.push(printObject);
+
+        }
+
+        cS += 1;
+
+    }
+
+    return newData;
+
+}
+
+function reconstruct(command){
+
+    var commands = [];
+
+    if(command === undefined){
+        return;
+    }
+
+    commands.push(command.commandCode);
+
+    if(command.x){
+        commands.push("X" + command.x);
+    }
+
+    if(command.y){
+        commands.push("Y" + command.y);
+    }
+
+    if(command.z){
+        commands.push("Z" + command.z * settings.heightMulitplier);
+    }
+
+    if(command.feed){
+        commands.push("F" + command.feed * settings.speedMultiplier);
+    }
+
+    if(command.extrusion){
+        
+        if(command.extrusion > 0){
+            commands.push("E" + command.extrusion * settings.extrusionMultiplier)
+        } else {
+            commands.push("E5");
+        }
+    }
+
+    if(command.other){
+        commands.push(command.other.join(" "));
+    }
+
+    commands = commands.join(' ');
+
+    return commands;
+
+}
+
+
+
+module.exports.deconstruct = deconstruct;
+module.exports.reconstruct = reconstruct;
--- print.js
+++ print.js
@@ -1,26 +1,34 @@
 var serialport = require("serialport"),
     fs = require('fs'),
-    argv = require('optimist').argv;
+    argv = require('optimist').argv,
+    GCODE = require('./GCODE/module.js');
 
 var printCommands = [],
     printPosition = 0;
 
-var settings = {
+GLOBAL.settings = {
     extrusionMultiplier : 20,
     heightMulitplier : 1,
     speedMultiplier : 1,
     printFile : undefined,
-    serialPort : "/dev/tty.usbserial-AH00WXOD",
+    serialPort : undefined,
     printing : false,
     paused : false
 }
 
 //===========================================================
 // Self executing function - Will run before the file load
-// Usage: node [options] print.js --file [FILE PATH] --speed [SPEED MULTIPLIER]
+// Usage: node [options] print.js --port [SERIAL PORT] --file [FILE PATH] --speed [SPEED MULTIPLIER]
 //===========================================================
 
 var handleArguments = (function(){
+
+    if (argv.port){
+        settings.serialPort = argv.port;
+    } else {
+        console.log("No serial port passed. Exiting");
+        process.exit(1);
+    }
 
     if(argv.file){
         settings.printFile = argv.file;
@@ -48,131 +56,6 @@ fs.readFile(settings.printFile, 'utf8', function (err,data) {
     printCommands = GCODE.deconstruct(data);
 
 });
-
-var GCODE = (function(){
-
-    function deconstruct(code){
-
-        var printData;
-
-        printData = code.split('\n');
-
-        var newData = [];
-
-        var cS = 0;
-
-        while(cS < printData.length){
-
-            var thisLine = printData[cS].split(";")[0].split(' ');
-
-            if(thisLine.length > 1){
-                var lL = 1;
-
-                var printObject = {};
-                printObject.commandCode = thisLine[0];
-                printObject.other = []
-
-
-                while(lL < thisLine.length){
-
-                    if(thisLine[lL].indexOf('E') != -1){
-                        var tempChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('E')).split('E')[1], 10);
-        
-                        if(tempChunk < 4){
-                            tempChunk = 4;
-                        }
-                        printObject.extrusion = tempChunk;
-
-                    } else if(thisLine[lL].indexOf('X') != -1){
-                        var xChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('X')).split('X')[1], 10);
-
-                        printObject.x = xChunk;
-                    } else if(thisLine[lL].indexOf('Y') != -1){
-                        var yChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('Y')).split('Y')[1], 10);
-
-                        printObject.y = yChunk;
-                    } else if(thisLine[lL].indexOf('Z') != -1){
-                        var zChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('Z')).split('Z')[1], 10);
-
-                        printObject.z = zChunk;
-                    } else if(thisLine[lL].indexOf('F') != -1){
-                        var fChunk = parseFloat(thisLine[lL].slice(thisLine[lL].indexOf('F')).split('F')[1], 10);
-
-                        printObject.feed = fChunk;
-                    } else {
-
-                        if(thisLine[lL] !== " " && thisLine[lL] !== ""){
-                            printObject.other.push(thisLine[lL]);
-                        }
-
-                    }
-
-                    lL += 1;
-                }
-
-                newData.push(printObject);
-
-            }
-
-            cS += 1;
-
-        }
-
-        return newData;
-
-    }
-
-    function reconstruct(command){
-
-        var commands = [];
-
-        if(command === undefined){
-            return;
-        }
-
-        commands.push(command.commandCode);
-
-        if(command.x){
-            commands.push("X" + command.x);
-        }
-
-        if(command.y){
-            commands.push("Y" + command.y);
-        }
-
-        if(command.z){
-            commands.push("Z" + command.z * settings.heightMulitplier);
-        }
-
-        if(command.feed){
-            commands.push("F" + command.feed * settings.speedMultiplier);
-        }
-
-        if(command.extrusion){
-            
-            if(command.extrusion > 0){
-                commands.push("E" + command.extrusion * settings.extrusionMultiplier)
-            } else {
-                commands.push("E5");
-            }
-        }
-
-        if(command.other){
-            commands.push(command.other.join(" "));
-        }
-
-        commands = commands.join(' ');
-
-        return commands;
-
-    }
-
-    return{
-        deconstruct : deconstruct,
-        reconstruct : reconstruct
-    };
-
-})();
 
 var SerialPort = serialport.SerialPort;
 var sp = new SerialPort(settings.serialPort, { 
